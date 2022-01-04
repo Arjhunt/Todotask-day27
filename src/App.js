95fe35479aed0bdcf5a6b08b70c8a66d74a209b8
Arjhun T <arjhun7204@gmail.com>
2022-01-04 13:33:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import deleteicon from './delete.png'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [taskInput, setTaskInput] = useState("")
+  const[toDolist,setTodolist]=useState([])
+
+//Add note fn
+  const addNote=()=>{
+    
+    toDolist.push({description: taskInput},{isComplete: false})
+    setTodolist(toDolist)
+    setTaskInput("")
+  }
+
+//delete fn
+const deleteTask=(index)=>{
+  const deletedItem=toDolist.filter((item,i)=>i!=index)
+  setTodolist(deletedItem)
 }
 
-export default App;
+
+//Complete fn
+const markComplete=(index)=>{
+  const list=[...toDolist]
+  list[index].isComplete= !list[index].isComplete;
+  setTodolist(list)
+}
+
+
+  return (
+    <div className='app-background'>
+      <p className='heading-text'>TO-DO LIST  📝</p>
+
+      <div className='task-container'>
+        <div>
+          <form>
+          <input className='text-input ' value={taskInput}
+            
+            onChange={(event) => setTaskInput(event.target.value)} required></input>
+
+
+          <button className='add-button' type= "submit" onClick={addNote}>ADD</button>
+          </form>
+        </div>
+
+        {toDolist?.length ? toDolist.map((inputObject,index)=>
+        <ListItem index={index} itemData={inputObject} deleteTask={deleteTask} markComplete={markComplete} />) : <p className='no-item-text'> 📌No task added </p>}
+        
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+function ListItem(props) {
+  return (
+    <div className='list-item row jc-space-between'>
+      <span className={props.itemData.isComplete? "task-complete" : ""}
+      onClick={()=>props.markComplete(props.index)}>{props.itemData.description}</span>
+
+      <img src={deleteicon} className='delete-icon' onClick={()=>props.deleteTask(props.index)} />
+    </div>
+  )
+}
+
+
+export default App
